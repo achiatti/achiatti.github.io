995536095dfd964381ff3a1b9a3570e44b0cd880
Agnese Chiatti <agnese.chiatti@gmail.com>
2020-07-03 16:20:47 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -39,6 +39,7 @@ $(document).ready(function () {
   if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
+    
 
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
