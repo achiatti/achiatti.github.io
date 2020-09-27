cab2e116694210e88af9c1aab8680352e254beac
Agnese Chiatti <agnese.chiatti@gmail.com>
2020-09-27 12:25:19 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,30 +5,19 @@ sitemap:
 ---
   $(document).ready(function () {
   
-      currentWidth = $('.panel-cover').width()
+      
       if (window.location.hash && window.location.hash === '#projects') {
         $('.panel-cover').addClass('panel-cover--collapsed');
         $('.content-wrapper.projects').removeClass('hidden').addClass('visible');
-        //$('a.pubs-button').removeClass('active');
-        //$('a.projects-button').addClass('active');
       }
       if (window.location.hash && window.location.hash === '#publications') {
         $('.panel-cover').addClass('panel-cover--collapsed right');
         $('.content-wrapper.pubs').removeClass('hidden').addClass('visible');
-        //$('a.projects-button').removeClass('active');
-        //$('a.pubs-button').addClass('active');
-      }
+             }
 
-      /*if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html'
-          && window.location.hash !== '#publications') {
-        $('.panel-cover').addClass('panel-cover--collapsed');
-        $('.content-wrapper.projects').removeClass('hidden').addClass('visible');
-        $('a.pubs-button').removeClass('active');
-        $('a.projects-button').addClass('active');
-      }*/
-
+  
     
-    $('a.projects-button').click(function (e) {
+   // $('a.projects-button').click(function (e) {
 
       /*if ($('.content-wrapper.projects').hasClass('showing')){
         $('.content-wrapper.projects').removeClass('animated slideInRight')
@@ -41,14 +30,10 @@ sitemap:
         return;
       }*/
 
-      currentWidth = $('.panel-cover').width()
+    /*  currentWidth = $('.panel-cover').width()
       $('.content-wrapper.pubs').removeClass('animated slideInRight').addClass('hidden');
       $('.content-wrapper.projects').removeClass('hidden');
-      //$('.panel-cover').addClass('panel-cover--collapsed')
-
-      //$('.content-wrapper.projects').addClass('animated slideInRight')
-
-
+     
        if (currentWidth < 960) {
           $('.panel-cover').addClass('panel-cover--collapsed')
           $('.content-wrapper.projects').addClass('animated slideInRight')
@@ -63,9 +48,9 @@ sitemap:
 
 
       })
+    */
     
-    
-    $('a.pubs-button').click(function (e) {
+   // $('a.pubs-button').click(function (e) {
 
       /*if ($('.content-wrapper.pubs').hasClass('showing')){
         $('.content-wrapper.pubs').removeClass('animated slideInRight')
@@ -77,7 +62,7 @@ sitemap:
         parent.location.hash = ''
         return;
       }*/
-       currentWidth = $('.panel-cover').width()
+     /*  currentWidth = $('.panel-cover').width()
        $('.content-wrapper.projects').removeClass('animated slideInRight').addClass('hidden');
       $('.content-wrapper.pubs').removeClass('hidden');
         if (currentWidth < 960) {
@@ -93,7 +78,7 @@ sitemap:
        $('a.pubs-button').addClass('active');
         $('.content-wrapper.pubs').addClass('showing');
 
-      })
+      })*/
 
    $('.btn-mobile-menu').click(function () {
       $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
@@ -102,10 +87,10 @@ sitemap:
     
     })
 
-   $('.navigation-wrapper .panel-button').click(function () {
+   /*$('.navigation-wrapper .panel-button').click(function () {
       $('.navigation-wrapper').toggleClass('visible')
       $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-    })
+    })*/
 
 })
     
