a6a470ed8c0d7b0a763845d9ee0cd787c178bfdf
Agnese Chiatti <agnese.chiatti@gmail.com>
2020-09-27 13:02:32 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -80,14 +80,14 @@ sitemap:
 
       })
 
-   /*$('.btn-mobile-menu').click(function () {
+   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
    $('.navigation-wrapper .panel-button').click(function () {
       $('.navigation-wrapper').toggleClass('visible')
       $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-    })*/
+    })
 
 })
     
