8dbd474ea8ea0702696f967ced0a2106929589ca
Agnese Chiatti <agnese.chiatti@gmail.com>
2020-09-27 14:47:55 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -120,7 +120,16 @@ sitemap:
   if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
-    
+  
+     $('.btn-mobile-menu').click(function () {
+    $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+  })
+
+  $('.navigation-wrapper .projects-button').click(function () {
+    $('.navigation-wrapper').toggleClass('visible')
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+  })
     
 })
     
